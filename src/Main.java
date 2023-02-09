e274c54a18fdf6f55321f23914f68973530a349d
gabriel1010 <101879808+ElCeacker@users.noreply.github.com>
2023-02-09 10:25:51 +0000

    experimento-tres

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,15 @@ import Huerto.Cesta;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Cesta cesta = new Cesta(10);
-        Granjero paco = new Granjero("Paco", 5, 2, cesta);
+        Granjero paco = new Granjero("Paco", 5, 5, cesta);
+        paco.setPriority(10);
         paco.start();
 
         Cliente gabri = new Cliente("Gabriel", 5, 5, cesta);
+        gabri.setPriority(1);
         gabri.start();
+
+
+
     }
 }
